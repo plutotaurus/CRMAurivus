30c164c024900d2feb59715bcbedbafc93e288be
Rianne <r.poppema@student.han.nl>
2024-11-29 21:38:06 +0100

    Set up spring boot

---
+++ backend/crm-aurivus/src/test/java/plutotaurus/crm_aurivus/CrmAurivusApplicationTests.java
@@ -0,0 +1,13 @@
+package plutotaurus.crm_aurivus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmAurivusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
